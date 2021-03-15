42af0acc1a4c15b7739735ce4eb426da363c3282
liquidiert <haberederkorbinian@googlemail.com>
2021-03-15 22:26:53 +0100

    feature server args :sparkles:
      - added arguments to server to be a little more general

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from xdo import Xdo
 import keyboard
 import asyncio
 import websockets
+import argparse
 
 xdo = Xdo()
 
@@ -10,15 +11,21 @@ async def handle_messages(websocket, path):
         xdo.activate_window(w_id)
         keyboard.press(message)
 
+def get_args():
+    parser = argparse.ArgumentParser(description="emuward - A simple key forwarder")
+    parser.add_argument("--ip", type=str, nargs=1, default="localhost", help="Ip to use")
+    parser.add_argument("--port", type=int, nargs=1, default=5387, help="Which port to use")
+    parser.add_argument("--appname", type=str, nargs=1, required=True, help="Name of the application xdotool should target")
+    return parser.parse_args()
 
 if __name__=="__main__":
 
-    w_id = xdo.search_windows(b"a7800")
+    w_id = xdo.search_windows(args.appname.encode())
     if len(w_id) > 1:
         raise RuntimeError("Only one emulator window should exist!")
     w_id = w_id[0]
-    
-    start_server = websockets.serve(handle_messages, "localhost", 5387)
-    
+
+    start_server = websockets.serve(handle_messages, args.ip, args.port)
+
     asyncio.get_event_loop().run_until_complete(start_server)
     asyncio.get_event_loop().run_forever()
